34c879eaf4f6288dca004655401a414461502021
Brahim Sioini <besioini@gmail.com>
2023-11-29 17:23:03 -0500

    practical loop 2

---
+++ data-collection.js
@@ -0,0 +1,46 @@
+const names = ["fight club", " ", " ","good will hunting", "Jerome", "Cynthia"];
+const greetings = [];
+
+// for (let i = 0; i < names.length; i++) {
+// 	greetings[i] = "Hello, " + names[i] + "!";
+// 	console.log(greetings[i]);
+// }
+
+
+// names.map(item => {
+//     console.log(item)
+// })
+
+
+// arrayName.filter((item) => {
+//    if(item === " "){
+//     return     
+//    }
+//    console.log(item)
+// })
+
+
+
+const vehicle1 = {
+	color: "blue",
+	year: 1989,
+	make: "Audi",
+	engine: {
+		cylinders: 6,
+		hp: 4000,
+		size: 3.2
+	}
+}
+
+const vehicle2 = {
+	color: "blue",
+	year: 1989,
+	make: "Audi",
+	engine: {
+		cylinders: 6,
+		hp: 4000,
+		size: 3.2
+	}
+}
+
+console.log(vehicle1 ===  vehicle2); // false
--- practical-loops.js
+++ practical-loops.js
@@ -2,36 +2,36 @@
 
 // Part 1: Fizz Buzz
 
-// for(let num=0; num<100; num++){
-//     if(num % 3 === 0){
-//        console.log('Fizz');
-//     }else if(num % 5 === 0){
-//         console.log('Buzz');
-//     }else if(num % 3 == 0 && num % 5 == 0 ){
-//         console.log('Fizz Buzz');
-//     }else
-//          console.log(num)
-// }
+for(let num=0; num<100; num++){
+    if(num % 3 === 0){
+       console.log('Fizz');
+    }else if(num % 5 === 0){
+        console.log('Buzz');
+    }else if(num % 3 == 0 && num % 5 == 0 ){
+        console.log('Fizz Buzz');
+    }else
+         console.log(num)
+}
 
 // Part 2: Prime Time
-// let num2 = 2; // Start from 2, as 0 and 1 are not prime
-// while (num2 < 20) {
-//     let isPrime = true;
-//     let factor = 2;
+let num2 = 2; // Start from 2, as 0 and 1 are not prime
+while (num2 < 20) {
+    let isPrime = true;
+    let factor = 2;
 
-//     while (factor <= Math.sqrt(num2)) {
-//         if (num2 % factor === 0) {
-//             isPrime = false;
-//             break;
-//         }
-//         factor++;
-//     }
+    while (factor <= Math.sqrt(num2)) {
+        if (num2 % factor === 0) {
+            isPrime = false;
+            break;
+        }
+        factor++;
+    }
 
-//     if (isPrime) {
-//         console.log(num2 + " is Prime");
-//     }
-//     num2++;
-// }
+    if (isPrime) {
+        console.log(num2 + " is Prime");
+    }
+    num2++;
+}
 
 // Part 3: Feeling Loopy
 
@@ -42,22 +42,24 @@ let row = [];
 for (let i = 0; i <csvData.length; i++) {
     let char = csvData[i];
 
-    // Check for line break
     if (char === '\r' && csvData[i + 1] === '\n') {
-        // Push the last cell to the row and log the row
         row.push(cell);
         console.log(...row);
 
-        // Reset for the next row
         cell = "";
         row = [];
-        i++; // Skip the '\n' after '\r'
+        i++; 
     } else if (char === ',') {
-        // Push the cell to the row and reset the cell
         row.push(cell);
         cell = "";
     } else {
-        // Add the character to the current cell
         cell += char;
     }
+}
+
+if (cell) {
+    row.push(cell);
+}
+if (row.length > 0) {
+    console.log(...row);
 }
